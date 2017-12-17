b4aff8fb452d3dcfd0db2591831a80a698a7a25f
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-17 17:26:09 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,7 +5,7 @@ def hello_t(array)
     yield array[i]
     i = i + 1
   end
-  hello_t(["Tim", "Tom", "Jim"]) do |name|
+  hello_t(array) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
